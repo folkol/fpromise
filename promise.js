bef06628a4e3d721b9642ecadda26d2d7b446125
Matte <mattias4@kth.se>
2016-08-23 08:46:03 +0200

    Started implementing, a few tests pass – time for cleanup.

--- promise.js
+++ promise.js
@@ -1,4 +1,52 @@
 /* jshint node: true */
 
-module.exports = function (executor) {
-};
+function Promise(executor) {
+    'use strict';
+    var pendingFulfills = [];
+    var pendingRejects = [];
+    var state = {
+        pending: true
+    };
+
+    var resolve = value => {
+        if (!state.pending) {
+            return;
+        }
+        state.value = value;
+        pendingFulfills.forEach(e => e());
+        state.pending = false;
+    };
+    var reject = error => {
+        if (state.value || state.error) {
+            return;
+        }
+        state.error = error;
+        pendingRejects.forEach(e => e());
+        state.pending = false;
+    };
+    var then = (onFulfilled, onRejected) => {
+        'use strict';
+        if (state.value) {
+            onFulfilled(state.value);
+        } else if (state.error) {
+            if (typeof onRejected === 'function') {
+                onRejected(state.error);
+            }
+        } else {
+            if (typeof onFulfilled === 'function') {
+                pendingFulfills.push(onFulfilled);
+            }
+            if (typeof onRejected === 'function') {
+                pendingRejects.push(onRejected);
+            }
+        }
+    };
+
+    executor(resolve, reject);
+
+    return {
+        'then': then
+    };
+}
+
+module.exports = Promise;
--- test-adapter.js
+++ test-adapter.js
@@ -1,7 +1,10 @@
-var MyPromise = require('./promise.js');
+var Promise = require('./promise.js');
 
-module.exports.deferred = () => ({
-    promise: new MyPromise(),
-    resolve: () => {},
-    reject: () => {}
-});
+module.exports.deferred = function () {
+    var result = {};
+    result.promise = new Promise((resolve, reject) => {
+        result.resolve = resolve;
+        result.reject = reject;
+    });
+    return result;
+};
